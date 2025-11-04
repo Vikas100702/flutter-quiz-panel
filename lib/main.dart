ad11a03067f3a9ed509544781406578c1e7febeb
VIKAS VERMA <vermasingh2002@gmail.com>
2025-11-04 11:03:36 +0530

    feat(core): Initialize Firebase and GoRouter

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quiz_panel/firebase_options.dart';
+import 'package:quiz_panel/providers/app_router_provider.dart';
+import 'package:quiz_panel/screens/auth/auth_wrapper.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    // Get the router from the provider
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      title: 'Online Quiz Panel',
+
+        // Connect the router
+        // We tell MaterialApp to use GoRouter for all routing.
+      routeInformationProvider: router.routeInformationProvider,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+    );
+  }
+}

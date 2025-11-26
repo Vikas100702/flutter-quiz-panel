e1d3ab720fe5d353ada3759be743c3905b9361ff
VIKAS VERMA <vermasingh2002@gmail.com>
2025-11-26 13:20:05 +0530

    feat: initialize dotenv in main.dart to load api keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:quiz_panel/firebase_options.dart';
 import 'package:quiz_panel/providers/app_router_provider.dart';
 
@@ -10,6 +11,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await dotenv.load(fileName: ".env");
 
   runApp(const ProviderScope(child: MyApp()));
 }
